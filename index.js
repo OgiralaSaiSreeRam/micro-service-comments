ca398ac3544c41fa3729c7a98714a54705f33f95
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-04-05 20:31:26 -0400

    Recording comments

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const {randomBytes}=require('crypto')
+const bodyParser= require('body-parser')
+
+const app=express()
+app.use(bodyParser.json())
+
+
+const commentsById={}
+
+app.get('/posts/:id/comments',(req,res,next)=>{
+    res.send(commentsById[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments',(req,res,next)=>{
+    const postId=req.params.id
+    console.log(postId);
+    const id=randomBytes(4).toString('hex')
+    const {content} = req.body
+    const comments=commentsById[postId] || []
+    
+
+    comments.push({
+        id,
+        content
+    })//identified by the post id and has an id for the individual comment as well.
+    
+    console.log(commentsById);
+
+    commentsById[postId]=comments
+
+    res.status(201).send(commentsById[postId])//send the comments of this post
+})
+
+app.listen(4001,()=>{
+    console.log('listening  on 4001');
+})
